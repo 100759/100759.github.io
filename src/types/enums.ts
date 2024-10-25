424cba512e54fdc930cc1c08e70bdd67f539487e
付恒 <2827162018@qq.com>
2024-10-25 19:29:52 +0800

    s

--- src/types/enums.ts
+++
@@ -1,8 +0,0 @@
-// 페이지 상태값 열거형 정의
-export enum PageStatus {
-    Deleted = 'Deleted', // 삭제됨
-    Writing = 'Writing', // 작성중
-    Ready = 'Ready', // 포스팅 준비됨
-    Updated = 'Updated', // 업데이트됨
-    ToBeDeleted = 'ToBeDeleted', // 삭제될 예정
-}
